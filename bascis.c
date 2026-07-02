3c8356960ca915baec6a1be12ae4272e78a1b3b2
Khushikumari8 <khushiyad113@gmail.com>
2026-07-02 19:49:15 +0530

    Create bascis.c

---
+++ bascis.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+printf("Computer is a electronic device.");
+printf("Program is a collection of statement.");
+printf("C program executes from Top to Bottom statement by statement.");
+return 0;
+}
