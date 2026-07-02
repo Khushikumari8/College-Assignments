c234c1c16243c4bb9f2a5e744d18fc9be3c3ac5f
Khushikumari8 <khushiyad113@gmail.com>
2026-07-02 20:07:27 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+// Variable declaration
+int n1,n2,n3;
+// accept numbers
+printf("Enter 1st number:");
+scanf("%d",&n1);
+printf("Enter 2nd number:");
+scanf("%d",&n2);
+// sum of the number
+n3=n1+n2;
+printf("sum=%d\n", n3);
+return 0;
+}
+
